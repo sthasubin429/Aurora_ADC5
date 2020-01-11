c90491d368badc85cf97724726616b36888d32d1
Rajesh Kawan <rakawang@gmail.com>
2020-01-11 13:22:20 +0545

    registration and templating part

--- Travelers_Journal/Travelers_Journal/settings.py
+++ Travelers_Journal/Travelers_Journal/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- Travelers_Journal/user/views.py
+++ Travelers_Journal/user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from  .forms import CustomForm 
+
 
 # Create your views here.
+
+def register(request):
+    form = CustomForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('register')       #change redirect path to homepage
+        else:
+            form = CustomForm()
+    return render(request, 'user/register.html', {'form' : form})
+
+def base(request):
+    return render(request, 'users/base.html')
