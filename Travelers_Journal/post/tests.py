d0404fa0b04e76b1ee3ae1ebdac0e8d028def5d9
subin <subinshrestha429@gmail.com>
2020-01-11 23:33:27 +0545

    Test Cases Updated

--- Travelers_Journal/post/tests.py
+++ Travelers_Journal/post/tests.py
@@ -5,13 +5,11 @@ from post.models import Posts
 from datetime import datetime
 
 
-class OrmTest(TestCase):
+class OrmBasicTest(TestCase):
     def setUp(self):
         Posts.objects.create(post_title="Test Title", post_content="Test Content", post_date=datetime.now())
         Posts.objects.create(post_title="Test Title1", post_content="Test Content1", post_date=datetime.now())
         Posts.objects.create(post_title="Test Title2", post_content="Test Content2", post_date=datetime.now())
-        Posts.objects.create(post_title="Test Title3", post_content="Test Content4  ", post_date=datetime.now())
-        
         
     def testORM(self):
         testPostObj = Posts.objects.get(post_title="Test Title")
@@ -21,3 +19,15 @@ class OrmTest(TestCase):
         self.assertEqual(testPostObj.post_content, "Test Content")
         self.assertIsNot(testPostObj.id, testPostObj1.id)
         self.assertIsNotNone(testPostObj)
+
+class OrmUpdateTest(TestCase):
+    def setUp(self):
+        Posts.objects.create(post_title="Test Title", post_content="Test Content", post_date=datetime.now())
+
+    def testORM(self):
+        testPostObj = Posts.objects.get(post_title="Test Title")
+        testPostObj.post_title = "Updated Title"
+        testPostObj.post_content = "Updated Content"
+        testPostObj.save()
+        self.assertEqual(testPostObj.post_title, "Updated Title")
+        self.assertEqual(testPostObj.post_content, "Updated Content")
