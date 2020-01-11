e734997567b639b6227f34cf46871a0b17bd630e
Rajesh Kawan <rakawang@gmail.com>
2020-01-11 23:40:55 +0545

    testcase commited

--- Travelers_Journal/user/tests.py
+++ Travelers_Journal/user/tests.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'travelersjournal',
+            'password': 'aurora'}
+        User.objects.create_user(**self.credentials)
+    def test_login(self):
+        # send login data
+        response = self.client.post('/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
