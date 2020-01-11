d96c63601e4612a550a0bb3371b3846e8e295015
Rajesh Kawan <rakawang@gmail.com>
2020-01-11 19:12:16 +0545

    registration and login and logout commited

--- Travelers_Journal/Travelers_Journal/urls.py
+++ Travelers_Journal/Travelers_Journal/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from user import views as user_view
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_view.register, name="register"),
     path('base/', user_view.base, name="base"),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 ]
--- Travelers_Journal/user/views.py
+++ Travelers_Journal/user/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from  .forms import CustomForm 
+from django.contrib.auth import login, logout, authenticate
 
-
-# Create your views here.
 
 def register(request):
-    form = CustomForm(request.POST)
     if request.method == "POST":
+        form = CustomForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('register')       #change redirect path to homepage
-        else:
-            form = CustomForm()
+            return redirect('base')       #change redirect path to homepage
+    else:
+        form = CustomForm()
     return render(request, 'user/register.html', {'form' : form})
 
 def base(request):
-    return render(request, 'users/base.html')
+    return render(request, 'user/base.html')
+
